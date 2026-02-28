7d7e159d5688849600495e58f11e8a5bed5ab194
rain <sorany.non@student.cadt.edu.kh>
2026-02-28 09:45:48 +0700

    imp: sidebar

---
+++ validations/user.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+  id: z.number(),
+  username: z.string(),
+  email: z.string(),
+  role: z.object({
+    id: z.number(),
+    name: z.string(),
+  }),
+});
+
+export type User = z.infer<typeof UserSchema>;
